20f6dd99443cbe02586c808ed4b65b11fd24ac0f
MinDucke <106810906+MinDucke@users.noreply.github.com>
2022-06-07 15:54:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,91 +1,52 @@
-import { useEffect, useState } from "react";
-
-import "./Header.css";
-import images from "../../../../assets/images";
 import Tippy from "@tippyjs/react/headless";
 
-import { Wrapper as PopperWrapper } from "../../../Popper";
-import AccountItem from "../../../AccountItem";
-import Button from "../../../Button";
-import Menu from "../../../Popper/Menu";
+import { Wrapper as PopperWrapper } from "../../Popper";
+import MenuItem from "./MenuItem";
+import Header from "./Header";
+import "./Menu.css";
+import { useState } from "react";
 
-const MENU_ITEMS = [
-  {
-    icon: <i class="fa-solid fa-language"></i>,
-    title: "English",
-    chidren: {
-      title: "language",
-      data: [
-        { code: "en", title: "English", type: 'language' },
-        { code: "vi", title: "Tiếng Việt", type: 'language' },
-      ],
-    },
-  },
-  {
-    icon: <i class="fa-solid fa-circle-question"></i>,
-    title: "Feedback and help",
-    to: "/feedback",
-  },
-  {
-    icon: <i class="fa-solid fa-keyboard"></i>,
-    title: "Keyboard shortcuts",
-  },
-];
-function Header() {
-  const [searchResult, setSearchResult] = useState([]);
+function Menu({ children, items = [] }) {
+  const [history, setHistory] = useState([{ data: items }]);
+  const current = history[history.length - 1];
 
-  useEffect(() => {
-    setTimeout(() => {
-      setSearchResult([]);
-    }, 5000);
-  }, []);
-
+  const renderItems = () => {
+    return current.data.map((item, index) => {
+      const isParent = !!item.children
+  console.log(isParent)
+      return (
+        <MenuItem
+          key={index}
+          data={item}
+          onClick={() => {
+            // console.log(item.children)
+            if (isParent) {
+              setHistory((prev) => [...prev, item.children]);
+              console.log(item.children)
+            }
+          }}
+        />
+      );
+    });
+  };
   return (
-    <header className="wrapper-header">
-      <div className="inner">
-        <div className="logo">
-          <img src={images.logo} alt="tiktok-logo" />
+    <Tippy
+      interactive
+      visible
+      delay={[0, 700]}
+      placement="bottom-end"
+      render={(attrs) => (
+        <div className="menu-list" tabIndex="-1" {...attrs}>
+          <PopperWrapper className="menu-popper">
+            <Header title={'Language'}/>
+            {renderItems()}
+          </PopperWrapper>
         </div>
-
-        <Tippy
-          interactive
-          visible={searchResult.length > 0}
-          render={(attrs) => (
-            <div className="search-result" tabIndex="-1" {...attrs}>
-              <PopperWrapper>
-                <h4 className="search-title"> Accounts</h4>
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-              </PopperWrapper>
-            </div>
-          )}
-        >
-          <div className="search">
-            <div className="box">
-              <div className="container-1">
-                <span className="icon">
-                  <i className="fa fa-search"></i>
-                </span>
-                <input type="search" id="search" placeholder="Search..." />
-              </div>
-            </div>
-          </div>
-        </Tippy>
-        <div className="action">
-          <Button text>Upload</Button>
-          <Button primary>Log in </Button>
-
-          <Menu items={MENU_ITEMS}>
-            <button className="more-btn">
-              <i class="fa-solid fa-ellipsis-vertical"></i>
-            </button>
-          </Menu>
-        </div>
-      </div>
-    </header>
+      )}
+    >
+      {children}
+    </Tippy>
   );
 }
 
-export default Header;
+export default Menu;
